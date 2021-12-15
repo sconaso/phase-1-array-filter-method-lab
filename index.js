690c2a013cd5866d584a39a32f5e84e50604862b
Scott Nason <scottmnason@gmail.com>
2021-12-14 22:19:33 -0700

    commit tree 8a2ff3409c580f1358b9ad4338c116a7f02d45a3

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution here
+function findMatching(arr, name){
+    const newArr = arr.filter(element => element.toUpperCase() === name.toUpperCase());
+    return newArr;
+}
+
+function fuzzyMatch(arr, partialName){
+    const newArr = arr.filter(element => element.substring(0,2) === partialName.substring(0,2));
+    return newArr;
+}
+
+function matchName(arr, objName){
+    const newArr = arr.filter(element => element.name === objName);
+    return newArr;
+}
